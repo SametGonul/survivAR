3e7b635e10a73a934703d9931878110ecc2d7883
Samet GÖNÜL <sametgonul95@gmail.com>
2018-04-03 22:09:10 +0300

    clan
    clan member info,clan requests

--- survivAR/Assets/Scripts/mastermember.cs
+++ survivAR/Assets/Scripts/mastermember.cs
@@ -6,7 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class mastermember : MonoBehaviour {
 
-    public Text requestText;
+    struct PlayerInfo
+    {
+        public string username;
+        public string playerID;
+        public string playerRole;
+        public string playerContribution;
+    }
+
+    PlayerInfo[] playerList = new PlayerInfo[5];
+
+
+    public Text[] membersInfo = new Text[5];
 
     string requestedName;
     int requestedID;
@@ -14,8 +25,38 @@ public class mastermember : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        int userID = PlayerPrefs.GetInt("userID");
+        WWWForm form = new WWWForm();
+        form.AddField("useridPost", userID);
+
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/clandetails.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            Debug.Log(ResultString);
+            string[] tokens1 = ResultString.Split(';');
+            int i;
+            for(i = 0; i < tokens1.Length; i++)
+            {
+                string[] tokens2 = tokens1[i].Split(' ');
+                PlayerInfo player = new PlayerInfo();
+                player.username = tokens2[0];
+                player.playerID = tokens2[1];
+                player.playerContribution = tokens2[2];
+                player.playerRole = tokens2[3];
+
+                playerList[i] = player;
+
+                membersInfo[i].text = playerList[i].username + "   " + playerList[i].playerContribution + "    " + playerList[i].playerRole;
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- survivAR/Assets/Scripts/requests.cs
+++ survivAR/Assets/Scripts/requests.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class requests : MonoBehaviour {
 
-    public Text requestText;
-	// Use this for initialization
-	void Start () {
+   
+    public Text requestText1;
+    public Text requestText2;
+    public Text requestText3;
+    public Text requestText4;
+    public Text requestText5;
+
+    private Text[] requestTexts = new Text[5];
+
+    public string[] tokens1;
+    string[] tokens2;
+    int masterID;
+    // Use this for initialization
+    void Start () {
+
+        masterID = PlayerPrefs.GetInt("userID");
+        int j;
+
+        requestTexts[0] = requestText1;
+        requestTexts[1] = requestText2;
+        requestTexts[2] = requestText3;
+        requestTexts[3] = requestText4;
+        requestTexts[4] = requestText5;
         WWWForm form = new WWWForm();
         int userID = PlayerPrefs.GetInt("userID");
         form.AddField("useridPost", userID);
@@ -24,14 +44,17 @@ public class requests : MonoBehaviour {
         {
             string ResultString = www.text;
             Debug.Log(ResultString);
-            string[] tokens1 = ResultString.Split(';');
-            string[] tokens2 = tokens1[0].Split(' ');
+            tokens1 = ResultString.Split(';');
+            int i;
+            for (i = 0; i < tokens1.Length; i++)
+            {
+                tokens2 = tokens1[i].Split(' ');
+                string requestedName = tokens2[0];
+                int requestedID = int.Parse(tokens2[1]);
+                int requestedPoint = int.Parse(tokens2[2]);
+                requestTexts[i].text = requestedName + "   " + requestedPoint;
+            }
 
-            string requestedName = tokens2[0];
-            int requestedID = int.Parse(tokens2[1]);
-            int requestedPoint = int.Parse(tokens2[2]);
-
-            requestText.text = requestedName + "   " + requestedID + "   " + requestedPoint;
         }
     }
 	
@@ -39,5 +62,224 @@ public class requests : MonoBehaviour {
 	void Update () {
 		
 	}
-    
+
+    public void acceptUser1()
+    {
+        Debug.Log("samet1");
+        tokens2 = tokens1[0].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/acceptmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+
+    }
+    public void acceptUser2()
+    {
+        Debug.Log("samet2");
+        tokens2 = tokens1[1].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/acceptmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void acceptUser3()
+    {
+        tokens2 = tokens1[2].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/acceptmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void acceptUser4()
+    {
+        tokens2 = tokens1[3].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/acceptmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void acceptUser5()
+    {
+        tokens2 = tokens1[4].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/acceptmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+
+    public void rejectUser1()
+    {
+        Debug.Log("samet3");
+        tokens2 = tokens1[0].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/rejectmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void rejectUser2()
+    {
+        tokens2 = tokens1[1].Split(' ');
+        
+        WWWForm form = new WWWForm();
+        Debug.Log(masterID);
+        Debug.Log(tokens2[1]);
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/rejectmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            Debug.Log(ResultString);
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void rejectUser3()
+    {
+        tokens2 = tokens1[2].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/rejectmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void rejectUser4()
+    {
+        tokens2 = tokens1[3].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/rejectmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+    public void rejectUser5()
+    {
+        tokens2 = tokens1[4].Split(' ');
+        WWWForm form = new WWWForm();
+        form.AddField("masteridPost", masterID);
+        form.AddField("useridPost", tokens2[1]);
+
+        WWW www = new WWW("http://cng491.000webhostapp.com/rejectmember2.php", form);
+
+        while (!www.isDone)
+        {
+
+        }
+        if (www.isDone)
+        {
+            string ResultString = www.text;
+            // 0 success,1 database error
+
+        }
+        Application.LoadLevel("requests");
+    }
+
 }
